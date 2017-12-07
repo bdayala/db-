b6edf1f7b7c7048035172df16e2b1b3e0e003edf
Bianca Ayala <bianca.ayala01@utrgv.edu>
2017-12-07 17:32:12 -0600

    Working signup pages
    To change html

--- emp_signup.php
+++ emp_signup.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$linked = mysqli_connect("localhost", "root", "", "daycare db");
+$link = mysqli_connect("localhost", "root", "", "daycare_db");
  
 // Check connection
-if($linked === false)
+if($link === false)
 {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
@@ -12,20 +13,17 @@ if($linked === false)
  
 print_r($_POST);
 print("<br>"); 
-print("<br>"); 
-print("<br>"); 
-
-$firname =$_POST['emp_fname'];
+$frname =$_POST['emp_fname'];
 $lsname = $_POST['emp_lname'];
 $ssocial = $_POST['emp_ssn'];
 $date_b = $_POST["d_o_b"];
-$emailad = $_POST["emailaddy"];
 $phonen = $_POST["ph_num"];
+$emailad = $_POST["emailaddy"];
 $addr = $_POST["addy"];
 
 
 // Attempt login query execution
-$sql = "INSERT INTO employee(firstName,lastName, employee_SSN, DOB ,email_address, phone_number, address) VALUES ('$firname', '$lsname','$ssocial','$date_b' ,'$emailad','$phonen' ,'$addr')";
+$sql = "INSERT INTO employee(firstName,lastName, employee_SSN, DOB ,email_address, phone_number, address) VALUES ('$frname', '$lsname','$ssocial','$date_b' ,'$emailad','$phonen' ,'$addr')";
 
 if(mysqli_query($linked, $sql))
 {
--- parent_signup.php
+++ parent_signup.php
@@ -1,4 +1,6 @@
+<!DOCTYPE html>
 <?php
+session_start();
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "daycare db");
@@ -34,3 +36,76 @@ if(mysqli_query($link, $sql))
 // Close connection
 mysqli_close($link);
 ?>
+
+
+
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <title>Parent Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+
+<body>
+	<div class="container">
+		<div class="starter-template">
+		<div class= "jumbotron">
+		<title>Parent Sign Up</title> 
+			<p>Please fill in your information to successfully sign up.</p>
+			
+		<form method="post">
+		<div class="form-group"> 
+			<label>First Name: <sup>*</sup></label>
+				<input type="text" name="parent_fname" class="form-control">
+				<span class="help-block"> </span>
+         </div>    
+	
+		<div class="form-group ">
+			<label>Last Name:<sup>*</sup></label>
+			<input type="text" name="parent_lname" class="form-control">
+			<span class="help-block"> </span>
+		</div>			
+			
+		<div class="form-group ">
+			<label> SSN:<sup>*</sup></label>
+			<input type="text" name="parent_ssn" class="form-control">
+			<span class="help-block"> </span>
+		</div>			
+			
+		<div class="form-group">
+			<label> DOB:<sup>*</sup></label>
+			<input type="text" name="d_o_b" class="form-control">
+			<span class="help-block"> </span>
+		</div>			
+			
+		<div class="form-group">
+			<label>Phone Number: <sup>*</sup></label>
+			<input type="text" name="ph_num" class="form-control">
+			<span class="help-block"> </span>
+		</div>	
+			
+		<div class="form-group">
+			<label>Email: <sup>*</sup></label>
+			<input type="text" name="emailaddy" class="form-control">
+			<span class="help-block"> </span>
+		</div>
+	
+		<div class="form-group ">
+			<label>Address:<sup>*</sup></label>
+			<input type="text" name="addy" class="form-control">
+			<span class="help-block"> </span>
+		</div>
+
+			<input type="submit" class="btn btn-primary" value="Submit">
+			
+		</form>
+
+	</div>
+</div>
+</body>
+</html>
+
